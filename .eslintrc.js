a43c2cc0d35888ed4777ed6959d00e2212e9eb89
NohJinho <jinho.noh@namutech.co.kr>
2023-12-12 14:39:03 +0900

    불필요한 파일 삭제 및 기본 파일 생성.
    MyPage, MyTable, MyForm의 세 개의 파일을 통하여 모달창에서 작성한 게시글이 실제 테이블에 반영되게 만든다.
    각각의 파일의 역할은 다음과 같다.
    
    MyPage : tableData를 보유하며, 새로운 데이터를 추가하는 addRow 기능을 정의한다.
    MyTable : tableData를 사용자 UI에 표시한다. 데이터가 변화할 경우 즉시 반응한다.
    MyForm : 기본적인 Modal창을 통하여 사용자의 입력을 받으며 MyPage에서 상속받은 addRow를 이용하여 데이터를 추가한다.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,48 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
-  ],
+  parser: "vue-eslint-parser",
   parserOptions: {
+    parser: "@typescript-eslint/parser",
     ecmaVersion: 2020,
+    sourceType: "module",
   },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:vue/vue3-recommended",
+  ],
+  plugins: ["prettier", "@typescript-eslint"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-unused-vars": "off", // 필수
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/multiline-html-element-content-newline": "off",
+    // Maximum 5 attributes per line instead of one
+    "vue/max-attributes-per-line": [
+      "error",
+      {
+        singleline: 8,
+      },
+    ],
+    "vue/html-self-closing": [
+      "error",
+      {
+        html: {
+          void: "always",
+          normal: "any",
+          component: "always",
+        },
+        svg: "always",
+        math: "always",
+      },
+    ],
   },
 };
